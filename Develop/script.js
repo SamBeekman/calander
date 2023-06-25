b45454afa20e0f2b9069bffbb1c126feef8a38af
Slug <sambeekman@hotmail.com>
2023-06-25 15:26:00 +1000

    fixed bg color, put local storage and time in loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,96 +1,60 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+function init() {
 
+  // WHEN I open the planner
+  // THEN the current day is displayed at the top of the calendar
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+  function timeDisplay() {
+    $('#currentDay').text('Today is: ' + dayjs().format('dddd, h:MM:ss a'));
+  }
+  setInterval(timeDisplay, 1000);
+  timeDisplay();
 
-$('#currentDay').text('Today is: ' + dayjs().format('dddd'));
+  // WHEN I scroll down
+  // THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+  // completed in HTML
 
+  // WHEN I view the timeblocks for that day
+  // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
-// did in HTML
+  const timeblockArray = [$('#hour-9'), $('#hour-10'), $('#hour-11'), $('#hour-12'), $('#hour-13'), $('#hour-14'), $('#hour-15'), $('#hour-16'), $('#hour-17')];
 
+  const currentTime = dayjs();
 
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+  for (let i = 0; i < timeblockArray.length; i++) {
+    let timeCheck = dayjs().hour(i + 9);
 
-//issue: sets all to 'future'
-
-const timeblockArray = [$('#hour-9'), $('#hour-10'), $('#hour-11'), $('#hour-12'), $('#hour-13'), $('#hour-14'), $('#hour-15'), $('#hour-16'), $('#hour-17')];
-for (let i = 0; i < 9; i++) {
-
-  let curentTime = dayjs().format('H');
-
-
-  for (let j = 9; j < 17; j++) {
-    let timeCheck = dayjs().hour(j);
-
-
-    if (dayjs(curentTime).isBefore(timeCheck)) {
-      timeblockArray[i].addClass('past');
-    } else if (dayjs(curentTime).isSame(timeCheck)) {
-      timeblockArray[i].addClass('present');
-    } else {
-      timeblockArray[i].addClass('future');
+    if (dayjs(timeCheck, 'h').isBefore(currentTime, 'h')) {
+      timeblockArray[i].addClass('past').removeClass('present future');
+    } else if (dayjs(timeCheck, 'h').isAfter(currentTime, 'h')) {
+      timeblockArray[i].addClass('future').removeClass('past present');
+    } else if (dayjs(timeCheck, 'h').isSame(currentTime, 'h')) {
+      timeblockArray[i].addClass('present').removeClass('past future');
     };
   };
+
+  // WHEN I click into a timeblock
+  // THEN I can enter an event
+  // was already functional
+
+  // WHEN I click the save button for that timeblock
+  // THEN the text for that event is saved in local storage
+
+  $('.btn').on('click', function (event) {
+    let userInput = $(event.target).closest('.time-block').find('.description').val();
+    let userID = $(event.target).closest('.time-block').attr('id');
+    localStorage.setItem(userID, userInput);
+  });
+
+  // WHEN I refresh the page
+  // THEN the saved events persist
+
+  for (let k = 9; k < 18; k++) {
+
+    if (localStorage.getItem(`hour-${k}`)) {
+      console.log(localStorage.getItem(`hour-${k}`));
+      $(`#hour-${k}`).find('.description').val(localStorage.getItem(`hour-${k}`));
+    }
+  }
 };
 
-
-
-
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-//can do already
-
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-$('.btn').on('click', function (event) {
-  let userInput = $(event.target).prev().val();
-  let userID = $(event.target).parent().attr('id');
-  localStorage.setItem(userID, userInput);
-});
-
-
-// WHEN I refresh the page
-// THEN the saved events persist
-$('#hour-9').children().eq(1).text(localStorage.getItem('hour-9'));
-$('#hour-10').children().eq(1).text(localStorage.getItem('hour-10'));
-$('#hour-11').children().eq(1).text(localStorage.getItem('hour-11'));
-$('#hour-12').children().eq(1).text(localStorage.getItem('hour-12'));
-$('#hour-13').children().eq(1).text(localStorage.getItem('hour-13'));
-$('#hour-14').children().eq(1).text(localStorage.getItem('hour-14'));
-$('#hour-15').children().eq(1).text(localStorage.getItem('hour-15'));
-$('#hour-16').children().eq(1).text(localStorage.getItem('hour-16'));
-$('#hour-17').children().eq(1).text(localStorage.getItem('hour-17'));
-
-//rewrite as a loop
-// for (let k = 9; k < 17; k++) {
-//     $('#hour-$[k]').children().eq(1).text(localStorage.getItem('user-input-hour-$[k]'));
-// }
+init();
